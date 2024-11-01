57ec398cd3c4287b4c507928b0f2f3b7f5fa1e2e
shoaibalirts <shoaibali.rts@gmail.com>
2024-11-01 20:44:13 +0100

    rendered picture and name of the track as per given id

--- src/app/playlist/[playlistID]/page.js
+++ src/app/playlist/[playlistID]/page.js
@@ -1,15 +1,19 @@
 import Playlisttracks from "@/components/playlisttracks";
 import { getTracksAgainstPlaylistId } from "@/lib/spotifyapi";
 export default async function PlaylistIdPage({ params }) {
-  const { id } = await params.playlistID;
-  console.log(id);
+  const { playlistID } = await params;
+
+  //   console.log(playlistID);
 
   // here we have to fetch tracks data instead of playlist-->page.js
   // fetch tracks
-  const tracks = await getTracksAgainstPlaylistId(id);
+  const tracks = await getTracksAgainstPlaylistId(playlistID);
   console.log(tracks);
 
+  //
+  const trackArr = [tracks.images[0].url, tracks.name, tracks.tracks.items];
+  console.log(trackArr);
+
   // now we have to fetch
-  return <Playlisttracks tracksData={tracks} />;
-  return null;
+  return <Playlisttracks tracksData={trackArr} />;
 }
--- src/components/playlisttracks.js
+++ src/components/playlisttracks.js
@@ -1,3 +1,16 @@
+"use client";
+
+import Image from "next/image";
+
 export default function Playlisttracks({ tracksData }) {
-  return "here is a table of all tracks/songs";
+  const [imgSrc, name, items] = tracksData;
+  console.log(name);
+  console.log(items);
+
+  return (
+    <section>
+      <Image src={imgSrc} alt={name} width={300} height={300} priority />
+      <p>{name}</p>
+    </section>
+  );
 }
