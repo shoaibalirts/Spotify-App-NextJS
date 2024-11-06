ddac741c939cb48e72bffa200b12b6e52e6334e6
shoaibalirts <shoaibali.rts@gmail.com>
2024-11-06 09:41:01 +0100

    removed signout route

--- src/app/signout/page.js
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-
-export default async function SignoutPage() {
-  const cookieStore = await cookies();
-  const myCookie = cookieStore.get("token_cookie");
-  return <Signout myCookie={myCookie} />;
-}
