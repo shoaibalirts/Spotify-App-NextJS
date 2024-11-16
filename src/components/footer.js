95813d76135dd02165e93a89e3e72ed330f0a7f1
shoaibalirts <shoaibali.rts@gmail.com>
2024-11-16 16:56:08 +0100

    refined footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,45 +5,59 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className={classes.footer}>
-      <Link href="/category">
+      <Link href="/category" className={classes.footericoncontainer}>
         <Image
+          className={classes.imgicon}
           src="/images/listfooter.svg"
-          width={20}
-          height={20}
+          width={30}
+          height={30}
           alt="categories icon"
           priority
         />
       </Link>
-      <Image
-        src="/images/microphonefooter.svg"
-        width={20}
-        height={20}
-        alt="microphone icon"
-        priority
-      />
-      <div className={classes.wififooter}>
+      <Link href="/category" className={classes.footericoncontainer}>
         <Image
+          className={classes.imgicon}
+          src="/images/microphonefooter.svg"
+          width={20}
+          height={20}
+          alt="microphone icon"
+          priority
+        />
+      </Link>
+      <Link
+        href="/featured"
+        className={`${classes.footericoncontainer} ${classes.wififooter}`}
+      >
+        <Image
+          className={classes.imgicon}
           src="/images/wififooter.svg"
           width={20}
           height={20}
           alt="wifi icon"
           priority
         />
-      </div>
-      <Image
-        src="/images/contrastfooter.svg"
-        width={20}
-        height={20}
-        alt="contrast icon"
-        priority
-      />
-      <Image
-        src="/images/settingfooter.svg"
-        width={20}
-        height={20}
-        alt="setting icon"
-        priority
-      />
+      </Link>
+      <Link href="/category" className={classes.footericoncontainer}>
+        <Image
+          className={classes.imgicon}
+          src="/images/contrastfooter.svg"
+          width={20}
+          height={20}
+          alt="contrast icon"
+          priority
+        />
+      </Link>
+      <Link href="/category" className={classes.footericoncontainer}>
+        <Image
+          className={classes.imgicon}
+          src="/images/settingfooter.svg"
+          width={20}
+          height={20}
+          alt="setting icon"
+          priority
+        />
+      </Link>
     </footer>
   );
 }
